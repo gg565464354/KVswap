18bc53aa73c06119d6067ab81a2cdf1dc3e8bff1
gg565464354 <1736986578@qq.com>
2025-12-11 15:46:17 +0800

    update

--- LongBench/pred.sh
+++ LongBench/pred.sh
@@ -1,24 +1,20 @@
-# !/bin/bash
+#!/bin/bash
+# 定义任务列表（已手动去掉 narrativeqa）
+tasks=("multifieldqa_en" "hotpotqa" "musique" "dureader" "gov_report" "samsum" "passage_retrieval_en" "lcc")
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <model_name> $1 <attn_type> $2 "
-    exit 1
-fi
-
-NUM_EXAMPLES=-1
-MODEL=${1}
-# TASK=${2}
-ATTN_TYPE=${2}
-# DTYPE=${4}
-# BUDGET_RATIO=${5}
-# ESTIMATE_RATIO=${6}
-
-RESULT_DIR="./results/pred/${MODEL}/${ATTN_TYPE}"
-RESULT_DIR_E="./results/pred_e/${MODEL}/${ATTN_TYPE}"
-
-echo "remove previous result file..."
-rm -f "${RESULT_DIR}/${TASK}.jsonl"
-rm -f "${RESULT_DIR_E}/${TASK}.jsonl"
-
-echo "Start to predict..."
-python -u pred.py --model ${MODEL} --method ${ATTN_TYPE} 
+# 循环执行
+for task in "${tasks[@]}"
+do
+    echo "=================================================="
+    echo "Start Running Task: $task"
+    echo "=================================================="
+    
+    python pred.py \
+        --model_name Qwen3-8B \
+        --method kvswap \
+        --kv_group_size 4 \
+        --kv_top_k_groups 250 \
+        --task "$task"
+        
+    echo "Finished Task: $task"
+done
